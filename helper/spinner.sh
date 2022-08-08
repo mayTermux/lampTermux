9435930dbecc1149dd4ba8c08724040655691f37
armandwipangestu <armandwi.pangestu7@gmail.com>
2022-08-08 20:18:54 +0000

    change icon when success and fail

--- helper/spinner.sh
+++ helper/spinner.sh
@@ -19,6 +19,8 @@
 #
 # Also see: test.sh
 
+OK="\u2714"
+FAIL="\u2718"
 
 function _spinner() {
     # $1 start/stop
@@ -64,9 +66,9 @@ function _spinner() {
             # inform the user uppon success or failure
             echo -en "\b["
             if [[ $2 -eq 0 ]]; then
-                echo -en "${green}${nc}"
+                echo -en "${green}${OK}${nc}"
             else
-                echo -en "${red}${nc}"
+                echo -en "${red}${FAIL}${nc}"
             fi
             echo -e "]"
             ;;
@@ -90,66 +92,3 @@ function stop_spinner {
     _spinner "stop" $1 $_sp_pid
     unset _sp_pid
 }
-
-
-function _spinner_backup() {
-
-    local on_success="found"
-    local on_fail="skip"
-    local white="\e[1;37m"
-    local green="\e[1;32m"
-    local red="\e[1;31m"
-    local yellow="\e[1;93m"
-    local nc="\e[0m"
-
-    case $1 in
-        start)
-            let column=$(tput cols)-${#2}-8
-            echo -ne ${2}
-            printf "%${column}s"
-
-            i=1
-            sp='\|/-'
-            delay=${SPINNER_DELAY:-0.2}
-
-            while :
-            do
-                printf "\b${sp:i++%${#sp}:1}"
-                sleep $delay
-            done
-            ;;
-        stop)
-            if [[ -z ${3} ]]; then
-                echo "spinner is not running.."
-                exit 1
-            fi
-
-            kill $3 > /dev/null 2>&1
-
-            echo -en "\b["
-            if [[ $2 -eq 0 ]]; then
-                echo -en "${green}${on_success}${nc}"
-            else
-                echo -en "${yellow}${on_fail}${nc}"
-            fi
-            echo -e "]"
-            ;;
-        *)
-            echo "invalid argument, try {start/stop}"
-            exit 1
-            ;;
-    esac
-}
-
-function start_spinner_backup {
-    _spinner_backup "start" "${1}" &
-    _sp_pid=$!
-    disown
-}
-
-function stop_spinner_backup {
-    _spinner_backup "stop" $1 $_sp_pid
-    unset _sp_pid
-}
-
-
